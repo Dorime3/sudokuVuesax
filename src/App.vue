b0f8b0ce9f09690ed907677239625f6f90f68aee
Денисов Родион <r.denisov@basicdecor.ru>
2022-08-08 18:35:08 +0300

    someDesignAdded

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,77 @@
 <template>
   <div>
-    <vs-button @click="resolveSudoku">Решить судоку</vs-button>
-    <transition name="fade">
+    <NavBar/>
+    <div class="sudoku-wrapper">
       <div class="grid-sudoku">
-
         <div v-for="(row, rowIdx) in table" :key="rowIdx" class="grid-row">
           <div v-for="(cell, colIdx) in row" :key="colIdx" class="grid-cell">
-            <transition-group tag="div" name="list-animation">
-              <input type="text" :key="colIdx" v-model="table[rowIdx][colIdx]" class="grid-cell-editor"/>
-            </transition-group>
+            <input type="text" :key="colIdx" v-model="table[rowIdx][colIdx]" class="grid-cell-editor"/>
           </div>
         </div>
       </div>
-    </transition>
+      <div style="display: flex">
+        <vs-button
+            color="rgb(59,222,200)"
+            gradient
+            @click="activeDialog = !activeDialog"
+            :disabled="!isShowLogs">
+          Показать логи
+        </vs-button>
+        <vs-button gradient danger @click="clearTable">Очистить поле</vs-button>
+        <vs-button gradient @click="resolveSudoku">Решить судоку</vs-button>
+      </div>
+      <vs-dialog blur v-model="activeDialog">
+        <template #header>
+          <h4>
+            Отслеживание логов
+          </h4>
+        </template>
+        <div>
+          {{ logs }}
+        </div>
+      </vs-dialog>
+    </div>
+    <div style="display: flex; justify-content: end; color:#ff9292; font-size: 12px">
+      @created by Родион
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
+import NavBar from "@/components/NavBar";
 
 export default {
+  components: {NavBar},
   data() {
     return {
       table: [
-        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-        [".", "9", "8", ".", ".", ".", ".", "6", "."],
-        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-        [".", "6", ".", ".", ".", ".", "2", "8", "."],
-        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ["5", "3", "", "", "7", "", "", "", ""],
+        ["6", "", "", "1", "9", "5", "", "", ""],
+        ["", "9", "8", "", "", "", "", "6", ""],
+        ["8", "", "", "", "6", "", "", "", "3"],
+        ["4", "", "", "8", "", "3", "", "", "1"],
+        ["7", "", "", "", "2", "", "", "", "6"],
+        ["", "6", "", "", "", "", "2", "8", ""],
+        ["", "", "", "4", "1", "9", "", "", "5"],
+        ["", "", "", "", "8", "", "", "7", "9"]
       ],
-      fuck: 0,
       tableSize: 9,
-      boxSize: 3
+      boxSize: 3,
+      activeDialog: false,
+      isShowLogs: false,
+      logs: ''
     }
   },
-  watch: {
-    fuck(val) {
-      console.log(val)
-    }
-  },
+  watch: {},
   methods: {
     findEmpty() {
       for (let r = 0; r < this.tableSize; r++) {
         for (let c = 0; c < this.tableSize; c++) {
-          if (this.table[r][c] === '.') {
+          if (this.table[r][c].length > 1) {
+            return 'error'
+          }
+          if (this.table[r][c] === '') {
             return [r, c]
           }
         }
@@ -87,10 +111,14 @@ export default {
         return true
       }
 
+      if (curPos === 'error') {
+        return false
+      }
+
       for (let i = 1; i <= this.tableSize; i++) {
         const curNum = i.toString();
         const isValid = this.validate(curNum, curPos);
-        console.log('curPos', curPos, 'curNum', curNum, 'isValid', isValid)
+        this.logs += `curPos, ${curPos}, curNum, ${curNum}, isValid, ${isValid}, `
 
         if (isValid) {
           const [x, y] = curPos;
@@ -102,7 +130,7 @@ export default {
           }
 
           // this.table[x][y] = '.'
-          Vue.set(this.table[x], y, '.')
+          Vue.set(this.table[x], y, '')
         }
 
       }
@@ -111,17 +139,59 @@ export default {
     },
 
     resolveSudoku() {
-      this.isSolved()
-      console.table(this.table)
+      // @todo сделать первичную валидацию на правильность ввода исходных данных
+      // в строках, столбцах и боксах не должны повторяться цифры
+
+      const result = this.isSolved()
+      this.logs += `isResult, ${this.table}`
+      this.isShowLogs = true;
+
+      if (result) {
+        this.$vs.notification({
+          color: 'success',
+          position: 'top-right',
+          duration: '8000',
+          title: 'Эксельсиор!',
+          text: 'Решение для вашего судоку найдено'
+        })
+      } else {
+        this.$vs.notification({
+          color: 'danger',
+          position: 'top-right',
+          duration: '8000',
+          title: 'Увы :(',
+          text: 'Этот судоку не решаем.'
+        })
+      }
+    },
+
+    clearTable() {
+      for (let r = 0; r < this.tableSize; r++) {
+        for (let c = 0; c < this.tableSize; c++) {
+          Vue.set(this.table[r], c, '')
+        }
+      }
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+.vs-notification__content__text p {
+  font-size: 14px;
+}
+
+.sudoku-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 40px);
+  flex-direction: column;
+}
+
 .grid-sudoku {
+  background: #d6e9ff;
   display: table;
-  background: white;
   border: 3px solid black;
 }
 
@@ -139,7 +209,13 @@ export default {
   border: 1px solid gray;
 }
 
+.grid-cell:hover {
+  background: #afd4ff;
+  border: 1px solid forestgreen;
+}
+
 .grid-cell-editor {
+  background: transparent;
   border: none;
   width: 20px;
   height: 20px;
@@ -148,5 +224,33 @@ export default {
   text-align: center;
   font-size: 18px;
   transition: all ease 1.0s;
+}
+
+.grid-cell-editor:focus {
+  color: forestgreen;
+}
+
+@media screen and (max-width: 600px) {
+  .grid-sudoku {
+    border: 2px solid black;
+  }
+
+  .grid-sudoku > div:nth-child(3), .grid-sudoku > div:nth-child(6) {
+    border-bottom: 2px solid black;
+  }
+
+  .grid-row > div:nth-child(3), .grid-row > div:nth-child(6) {
+    border-right: 2px solid black;
+  }
+
+  .grid-cell {
+    padding: 5px;
+  }
+
+  .grid-cell-editor {
+    width: 14px;
+    height: 10px;
+    font-size: 12px;
+  }
 }
 </style>
