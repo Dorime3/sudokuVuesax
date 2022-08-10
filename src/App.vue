eca4aae5e7a41a10a155523ca910789e3a6312e7
Денисов Родион <r.denisov@basicdecor.ru>
2022-08-10 16:46:06 +0300

    ready_to_deploy

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,75 @@
   <div>
     <NavBar/>
     <div class="sudoku-wrapper">
+      <div style="display: flex; align-items: center">
+        <h1 class="main-title">
+          Решалкин судок
+        </h1>
+        <div style="display: inline-block; margin: 5px 0 0 10px">
+          <vs-tooltip shadow bottom not-hover v-model="showInfo">
+            <vs-button
+                icon
+                warn
+                gradient
+                animation-type="rotate"
+                @click="showInfo=!showInfo"
+            >
+              <i class='bx bx-cog'></i>
+              <template #animate>
+                <i class='bx bxs-cog'></i>
+              </template>
+            </vs-button>
+            <template #tooltip>
+              <div class="content-tooltip">
+                <div class="body">
+                  <vs-avatar circle size="80" ref="avatarContent">
+                    <img
+                        src="https://banner2.cleanpng.com/20180528/lyp/kisspng-citron-cactus-m-cartoon-flowerpot-prickly-5b0cb1d8b18e71.3609451315275586167273.jpg"
+                        @load="closeLoading"
+                        alt="">
+                  </vs-avatar>
+                </div>
+                <footer>
+                  Для поиска решения используется алгоритм DFS(в глубину), рекурсивно вызывающий функцию обратного
+                  вызова для перебора всех подходящих значений для каждой ячейки таблицы.
+                  Натягивая сову на глобус, я смог провалидировать вводимые данные и извещать об ошибке пользователя. Но
+                  не все 😐. Поэтому есть вероятность "не выхода" из бесконечного цикла, что приведет к забитию
+                  колстека, выделяемым вкладке
+                  браузером.
+                  <div style="display: flex; justify-content: center">
+                    <vs-button
+                        gradient
+                        @click="showInfo=!showInfo">
+                      Я понял, давай пока 👋
+                    </vs-button>
+                  </div>
+                </footer>
+              </div>
+            </template>
+          </vs-tooltip>
+        </div>
+      </div>
       <div class="grid-sudoku">
         <div v-for="(row, rowIdx) in table" :key="rowIdx" class="grid-row">
           <div v-for="(cell, colIdx) in row" :key="colIdx" class="grid-cell">
-            <input type="text" :key="colIdx" v-model="table[rowIdx][colIdx]" class="grid-cell-editor"/>
+            <input type="text" :key="colIdx" :value="table[rowIdx][colIdx]" @input="setNumber($event ,rowIdx, colIdx)"
+                   class="grid-cell-editor"/>
           </div>
         </div>
       </div>
-      <div style="display: flex">
-        <vs-button
-            color="rgb(59,222,200)"
-            gradient
-            @click="activeDialog = !activeDialog"
-            :disabled="!isShowLogs">
-          Показать логи
-        </vs-button>
+      <div style="display: flex; margin-top: 5px">
+        <vs-tooltip bottom color="rgb(59,222,200)">
+          <vs-button
+              color="rgb(59,222,200)"
+              gradient
+              @click="activeDialog = !activeDialog"
+              :disabled="!isShowLogs">
+            Показать логи
+          </vs-button>
+          <template #tooltip>
+            Куча безполезной инфы
+          </template>
+        </vs-tooltip>
         <vs-button gradient danger @click="clearTable">Очистить поле</vs-button>
         <vs-button gradient @click="resolveSudoku">Решить судоку</vs-button>
       </div>
@@ -30,9 +84,22 @@
           {{ logs }}
         </div>
       </vs-dialog>
+      <div class="promo-block">
+        <PromoBlock/>
+      </div>
     </div>
-    <div style="display: flex; justify-content: end; color:#ff9292; font-size: 12px">
-      @created by Родион
+    <div style="display: flex; justify-content: space-between">
+      <div style="width: 50px">
+        <vs-switch dark v-model="darkTheme" @click="changeTheme">
+          <template #circle>
+            <i v-if="!darkTheme" class='bx bxs-sun'></i>
+            <i v-else class='bx bxs-moon' style="color: #fff"></i>
+          </template>
+        </vs-switch>
+      </div>
+      <div style="font-size: 12px; display: flex; align-items: flex-end;">
+        <p>@created by Родион</p>
+      </div>
     </div>
   </div>
 </template>
@@ -40,9 +107,10 @@
 <script>
 import Vue from 'vue';
 import NavBar from "@/components/NavBar";
+import PromoBlock from "@/components/PromoBlock";
 
 export default {
-  components: {NavBar},
+  components: {PromoBlock, NavBar},
   data() {
     return {
       table: [
@@ -59,12 +127,74 @@ export default {
       tableSize: 9,
       boxSize: 3,
       activeDialog: false,
+      globalLoading: null,
       isShowLogs: false,
-      logs: ''
+      showInfo: false,
+      darkTheme: false,
+      logs: '',
+      loadingAvatar: null
     }
   },
-  watch: {},
+  watch: {
+    showInfo(val) {
+      setTimeout(() => {
+        if (val) {
+          this.loadingAvatar = this.$vs.loading({
+            target: this.$refs.avatarContent,
+            type: 'waves',
+            color: '#d5397b',
+          })
+        }
+      }, 0)
+    },
+  },
+  mounted() {
+    const currentTheme = localStorage.getItem('vsTheme')
+    console.log('currentTheme', currentTheme)
+    if (currentTheme !== 'light') {
+      this.darkTheme = true;
+      this.$vs.toggleTheme('light');
+      document.body.classList.add('darken')
+      document.body.style.background = '#18191c'
+      document.body.style.color = '#fff'
+      localStorage.setItem('vsTheme', 'dark')
+    }
+    // loader
+    this.globalLoading = this.$vs.loading({
+      type: 'waves',
+      color: '#d5397b'
+    })
+
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        this.globalLoading.close();
+      }, 1000)
+    })
+  },
   methods: {
+    closeLoading() {
+      this.loadingAvatar.close()
+    },
+    setNumber(e, row, col) {
+      let currentVal = e.target.value[e.target.value.length - 1]
+      currentVal = currentVal.match(/[1-9]/) ? currentVal : '';
+      Vue.set(this.table[row], col, currentVal)
+    },
+    changeTheme() {
+      this.toggleTheme(this.darkTheme ? 'dark' : 'light')
+    },
+    toggleTheme(theme) {
+      this.$vs.toggleTheme(theme);
+      if (theme === 'light') {
+        document.body.classList.add('darken')
+        document.body.style.background = '#18191c'
+        document.body.style.color = '#fff'
+      } else {
+        document.body.classList.remove('darken')
+        document.body.style.background = 'none'
+        document.body.style.color = 'rgb(44, 62, 80)'
+      }
+    },
     findEmpty() {
       for (let r = 0; r < this.tableSize; r++) {
         for (let c = 0; c < this.tableSize; c++) {
@@ -177,15 +307,22 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
+
+body {
+  margin-bottom: 0;
+}
+
 .vs-notification__content__text p {
   font-size: 14px;
 }
 
 .sudoku-wrapper {
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: calc(100vh - 40px);
+  height: calc(100vh - 48px);
   flex-direction: column;
 }
 
@@ -230,6 +367,30 @@ export default {
   color: forestgreen;
 }
 
+.promo-block {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
+.main-title {
+  font-family: 'Amatic SC', cursive;
+  font-size: 48px;
+}
+
+.content-tooltip .body {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.content-tooltip .body .vs-avatar-content {
+  margin-top: -50px;
+  border: 3px solid transparent;
+  box-shadow: 0 4px 15px 0 rgba(0, 0, 0, .1);
+}
+
+
 @media screen and (max-width: 600px) {
   .grid-sudoku {
     border: 2px solid black;
@@ -248,9 +409,24 @@ export default {
   }
 
   .grid-cell-editor {
-    width: 14px;
-    height: 10px;
-    font-size: 12px;
+    width: 20px;
+    height: 20px;
+    font-size: 18px;
+  }
+
+  .main-title {
+    font-family: 'Amatic SC', cursive;
+    font-size: 36px;
   }
 }
+
+
+/*[vs-theme=dark] {*/
+/*  --vs-theme-bg: #18191c;*/
+/*  --vs-theme-color: #fff;*/
+/*  --vs-theme-layout: #1e2023;*/
+/*  --vs-theme-bg2: #141417;*/
+/*  --vs-theme-code: #141417;*/
+/*  --vs-theme-code2: #161619;*/
+/*}*/
 </style>
